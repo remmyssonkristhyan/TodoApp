f132a3e518df8d6ae49b3d3cb0c8b225397bbed4
remmyssonkristhyan <rkristhyan@gmail.com>
2022-09-19 00:55:45 -0300

    connect to database

--- app/src/main/java/TodoApp/App.java
+++ app/src/main/java/TodoApp/App.java
@@ -3,6 +3,9 @@
  */
 package TodoApp;
 
+import java.sql.Connection;
+import util.ConnectionFactory;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
@@ -10,5 +13,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        Connection c = ConnectionFactory.getConnection();
+        ConnectionFactory.closeConnection(c);
     }
 }
